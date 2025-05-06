6d1e85692e7eddb77d145c1db0c066bc7587f212
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2025-05-06 11:53:40 +0530

    hardcode ip

--- src/Components/Ip_phones/Sipserver.js
+++ src/Components/Ip_phones/Sipserver.js
@@ -15,7 +15,7 @@ export default function SipServer() {
   const [loading, setLoading] = useState(false);
   const [fileInputKey, setFileInputKey] = useState(Date.now());
   const navigate = useNavigate();
-  const BaseUrlSpring = window.location.host.split(":")[0] || "localhost";
+  const BaseUrlSpring = "192.168.250.58" || "localhost";
   const PORTSpring = process.env.REACT_APP_API_SPRING_PORT || "9093";
   const CookieName = process.env.REACT_APP_COOKIENAME || "auto provision";
   const Token = Cookies.get(CookieName);
